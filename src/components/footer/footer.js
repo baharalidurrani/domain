30054746ccad930ace7509735b192ad086518664
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-10-31 13:24:18 +0500

    powered by in progress

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,8 +6,42 @@ class Footer extends React.Component {
     return (
       <div className="footer">
         <div className="content">
-          <div class="center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <div className="center">
+            <span>Powered By</span>
+            <br />
+            <a
+              href="http://gitlab.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e1/GitLab_logo.svg"
+                height="40vh"
+                alt=""
+              />
+            </a>
+            <a
+              href="https://cloud.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/61/Google_Cloud_Logo.svg"
+                height="40vh"
+                alt=""
+              />
+            </a>
+            <a
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+                height="40vh"
+                alt=""
+              />
+            </a>
           </div>
         </div>
         <div className="copyRight">
