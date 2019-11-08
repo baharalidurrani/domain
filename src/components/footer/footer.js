cd89adc866a2239ebe6230259b92edbcab5b58bb
Bahar Ali Durrani <baharalidurrani@gmail.com>
2019-11-08 11:40:04 +0500

    serviceWorker unregistered | footer logos changed

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -22,19 +22,19 @@ class Footer extends React.Component {
             </a>
             <a
               className="powerLogo"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={ReactLogo} height="40px" alt="" />
-            </a>
-            <a
-              className="powerLogo"
               href="https://firebase.google.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={FirebaseLogo} height="40px" alt="" />
+            </a>
+            <a
+              className="powerLogo"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={ReactLogo} height="40px" alt="" />
             </a>
           </div>
         </div>
