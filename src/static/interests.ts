7e693f8c18e9af86de0e9dcbc10699f4d1387f5b
baharalidurrani <baharalidurrani@gmail.com>
2020-07-31 03:35:22 -0700

    repetition removed

--- src/static/interests.ts
+++ src/static/interests.ts
@@ -2,14 +2,13 @@ export default [
   { name: "Python", url: "https://www.python.org/" },
   { name: "Ruby", url: "https://www.ruby-lang.org/" },
   { name: "Rust", url: "https://www.rust-lang.org/" },
-  { name: "Rust", url: "https://www.rust-lang.org/" },
   { name: "WebAssembly", url: "https://webassembly.org/" },
   { name: "Go", url: "https://golang.org/" },
   { name: "Vue", url: "https://vuejs.org/" },
   { name: "Angular", url: "https://angular.io/" },
   { name: "Deep Learning", url: "https://wikipedia.org/wiki/Deep_learning" },
   { name: "TensorFlow", url: "https://www.tensorflow.org/" },
-  { name: "Microservices", url: "https://wikipedia.org/wiki/Microservices" },
+  { name: "MicroServices", url: "https://wikipedia.org/wiki/Microservices" },
   { name: "Kubernetes", url: "https://kubernetes.io/" },
   { name: "Blockchain", url: "https://wikipedia.org/wiki/Blockchain" }
 ];
